b28b0496e1d755ef027f679a537f585503503fce
CleberNandi <cleber.nandi@gmail.com>
2021-04-30 15:01:24 -0300

    Ajustado settings, Adicionado home.css

--- chameleon/settings/base.py
+++ chameleon/settings/base.py
@@ -13,8 +13,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from pathlib import Path
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 
 
@@ -63,7 +61,7 @@ ROOT_URLCONF = 'chameleon.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
